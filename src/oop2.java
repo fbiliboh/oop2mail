500b90e634a1cb5b058302541797cda4bbd59049
fbiliboh <phil.bad@gmx.ch>
2016-03-14 21:41:08 +0100

    test push function

--- src/oop2.java
+++ src/oop2.java
@@ -1,12 +1,44 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 import static javafx.application.Application.launch;
 
 /**
  * Created by Skynet on 06.03.2016.
  */
-public class oop2 {
+public class oop2 extends Application {
 
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Send Mail");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("sent E-Mail");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
-
 }
