d0485ff456aa22809b114529a75e03c50ab07cb4
TylerTheHumanCompiler <mc.twist@hotmail.com>
2016-03-14 19:54:13 +0100

    Initial commit

---
+++ src/oop2.java
@@ -0,0 +1,12 @@
+import static javafx.application.Application.launch;
+
+/**
+ * Created by Skynet on 06.03.2016.
+ */
+public class oop2 {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
