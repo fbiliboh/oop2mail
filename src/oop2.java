682cc17dafae3b3f387ad3dbe8fd29d6d780bdb8
fbiliboh <phil.bad@gmx.ch>
2016-03-15 11:35:37 +0100

    small layout update 3

--- src/oop2.java
+++ src/oop2.java
@@ -7,10 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
@@ -24,12 +21,11 @@ public class oop2 extends Application {
     private TextField mailAdress, mailSubject;
     private TextArea mailTextArea;
 
-
     @Override
     public void start(Stage primaryStage) {
 
         BorderPane borderPane = new BorderPane();
-                   borderPane.setCenter(createCenterGroup());
+                   borderPane.setCenter(allContent());
                    //borderPane.setRight(createSendButton());
                    borderPane.setPadding(new Insets(15,15,15,15));
 
@@ -44,10 +40,40 @@ public class oop2 extends Application {
         primaryStage.show();
     }
 
+    private HBox allContent(){
+
+        HBox allContent = new HBox(2);
+             allContent.getChildren().addAll(createCenterGroup(),createSideMenu());
+
+        return allContent;
+    }
+
     private VBox createCenterGroup() {
-        VBox controlBox = new VBox(5, createHeaderBox(),
+        VBox controlBox = new VBox(2, createFinalHeaderBox(),
                                       createTextArea());
         return controlBox;
+    }
+
+    private VBox createSideMenu() {
+
+        Button btn = new Button("Fischen");
+
+        VBox sideMenuButtons = new VBox(5);
+             sideMenuButtons.getChildren().addAll(btn);
+             //sideMenuButtons.getChildren().addAll(sidebar.getControlButton(), iconOption);
+             //sideMenuButtons.setPadding(new Insets(35, 5, 10, 5));
+
+        return sideMenuButtons;
+    }
+
+    private HBox createFinalHeaderBox() {
+
+        HBox headerBox = new HBox(2);
+             headerBox.setPadding(new Insets(10,10,10,10));
+             headerBox.getStyleClass().add("vbox");
+             headerBox.getChildren().addAll(createHeaderTextFields(),createSendButton());
+
+        return headerBox;
     }
 
     private TextArea createTextArea() {
@@ -55,29 +81,12 @@ public class oop2 extends Application {
         mailTextArea = new TextArea();
         mailTextArea.setPrefSize(Double.MAX_EXPONENT,Double.MAX_EXPONENT);
         mailTextArea.setFocusTraversable(true);
+        mailTextArea.setWrapText(true);
+        mailTextArea.setStyle("-fx-control-inner-background: black; -fx-text-fill: white"); //set as ID with styling.css!
         //mailTextArea.requestFocus();
+        mailTextArea.getStyleClass().add("vbox");
 
         return mailTextArea;
-    }
-
-    private HBox createHeaderBox() {
-
-        HBox headerBox = new HBox(2);
-             headerBox.getChildren().addAll(createHeaderTextFields(),createSendButton());
-
-        return headerBox;
-    }
-
-
-    private Button createSendButton() {
-
-        sendButton = new Button();
-        sendButton.setText("Send");
-        sendButton.setMinSize(50,60);
-
-        sendButton.setOnAction(event -> System.out.println("sent E-Mail"));
-
-        return sendButton;
     }
 
     private VBox createHeaderTextFields() {
@@ -93,6 +102,17 @@ public class oop2 extends Application {
              headerVBox.getChildren().addAll(mailAdress,mailSubject);
 
         return headerVBox;
+    }
+
+    private Button createSendButton() {
+
+        sendButton = new Button();
+        sendButton.setText("Send");
+        sendButton.setMinSize(50,60);
+
+        sendButton.setOnAction(event -> System.out.println("sent E-Mail"));
+
+        return sendButton;
     }
 
     /**
