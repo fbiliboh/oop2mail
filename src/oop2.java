38cd2cb534b0d6f948442bf68685bf0f03e650c6
TylerTheHumanCompiler <mc.twist@hotmail.com>
2016-03-15 15:03:07 +0100

    Email sände geit (immerno), Benutzername, Password und BFH-email-Name chöi ir klass UserVariables erstetzt wärde
    Usserdäm:
    MIME message (sprich ds eigentlech e-Mail) jetzt mit HTML!!!
    theoretisch... i due aber aui spezialzeiche usm TextArea "escape", auso chasch no kenni tags innerhalb vor nachricht bruche wiu das när i html eifach wie text dargsteut wird.
    
    was me aber fäng cha isch html usserhalb dr nachricht (davor und danach) bruche...:
     ir BFH_EmalSender.java hets ir send methode drü strings, mit de näme: font, fontsize, und fontcolor :)

--- src/oop2.java
+++ src/oop2.java
@@ -42,7 +42,7 @@ public class oop2 extends Application {
     public void start(Stage primaryStage) throws MessagingException, ExecutionException, InterruptedException {
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(createCenterGroup());
+        borderPane.setCenter(allContent());
         //borderPane.setRight(createSendButton());
         borderPane.setPadding(new Insets(15, 15, 15, 15));
 
@@ -55,57 +55,71 @@ public class oop2 extends Application {
         primaryStage.setTitle("");
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-
         Label labelObservable = new Label();
         Label labelOldvalue = new Label();
         Label labelNewvalue = new Label();
 
         Model1 model1 = new Model1();
 
-        model1.stringProperty.addListener(new ChangeListener() {
-            @Override
-            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                labelObservable.setText((String) observable.getValue());
-                labelOldvalue.setText((String) oldValue);
-                labelNewvalue.setText((String) newValue);
+        model1.stringProperty.addListener(new
 
-                if (oldValue.equals(newValue) == false) {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            String incoming = newValue.toString();
-                            if (incoming.contains("-fx-border-color: #C0C0C0;") == false && incoming.contains("-fx-border-color: #c0c0c0;") == false) {
-                                String redhex;
-                                String bluehex;
-                                if(Integer.toHexString(hxv * 3).length() == 1) {
-                                    redhex = new String("0" + Integer.toHexString(hxv * 3));
-                                } else {
-                                    redhex = Integer.toHexString(hxv * 3);
+                                                  ChangeListener() {
+                                                      @Override
+                                                      public void changed (ObservableValue observable, Object oldValue, Object newValue){
+                                                          labelObservable.setText((String) observable.getValue());
+                                                          labelOldvalue.setText((String) oldValue);
+                                                          labelNewvalue.setText((String) newValue);
 
-                                }
-                                if(hxv == 0) {bluehex = Integer.toHexString(255);} else {bluehex = Integer.toHexString(256 - hxv);}
+                                                          if (oldValue.equals(newValue) == false) {
+                                                              Platform.runLater(new Runnable() {
+                                                                  @Override
+                                                                  public void run() {
+                                                                      String incoming = newValue.toString();
+                                                                      if (incoming.contains("-fx-border-color: #C0C0C0;") == false && incoming.contains("-fx-border-color: #c0c0c0;") == false) {
+                                                                          String redhex;
+                                                                          String bluehex;
+                                                                          if (Integer.toHexString(hxv * 3).length() == 1) {
+                                                                              redhex = new String("0" + Integer.toHexString(hxv * 3));
+                                                                          } else {
+                                                                              redhex = Integer.toHexString(hxv * 3);
+
+                                                                          }
+                                                                          if (hxv == 0) {
+                                                                              bluehex = Integer.toHexString(255);
+                                                                          } else {
+                                                                              bluehex = Integer.toHexString(256 - hxv);
+                                                                          }
 
 
-                                String hexborder = new String("-fx-font-size: 23px; -fx-btext-alignment: center; -fx-border-color: #" + redhex + bluehex + redhex + "; -fx-border-width: 3px; -fx-effect: dropshadow(gaussian, black, 10, 3.0, 100, 100); -fx-alignment: center; -fx-text-fill: #" + redhex + bluehex + redhex + "; -fx-fill-width: true;");
-                                mailTextArea.setStyle(hexborder);
-                                hxv++;
+                                                                          String hexborder = new String("-fx-font-size: 23px; -fx-btext-alignment: center; -fx-border-color: #" + redhex + bluehex + redhex + "; -fx-border-width: 3px; -fx-effect: dropshadow(gaussian, black, 10, 3.0, 100, 100); -fx-alignment: center; -fx-text-fill: #" + redhex + bluehex + redhex + "; -fx-fill-width: true;");
+                                                                          mailTextArea.setStyle(hexborder);
+                                                                          hxv++;
 
-                            } else {
-                                mailTextArea.setStyle("-fx-font-size: 10px; -fx-text-alignment: left; -fx-border-color: #C0C0C0; -fx-border-width: 0px;");
-                                mailTextArea.clear();
-                                hxv = 0;
-                            }
+                                                                      } else {
+                                                                          mailTextArea.setStyle("-fx-font-size: 10px; -fx-text-alignment: left; -fx-border-color: #C0C0C0; -fx-border-width: 0px;");
+                                                                          mailTextArea.clear();
+                                                                          hxv = 0;
+                                                                      }
 
-                        }
+                                                                  }
 
-                    });
-                }
-            }
-        });
+                                                              });
+                                                          }
+                                                      }
+                                                  }
+
+        );
         model1.start();
     }
+
+
+    private HBox allContent() throws InterruptedException, ExecutionException, MessagingException {
+
+        HBox allContent = new HBox(2);
+        allContent.getChildren().addAll(createCenterGroup(), createSideMenu());
+        return allContent;
+    }
+
 
     private VBox createCenterGroup() throws MessagingException, ExecutionException, InterruptedException {
         VBox controlBox = new VBox(5, createHeaderBox(),
@@ -113,14 +127,61 @@ public class oop2 extends Application {
         return controlBox;
     }
 
+    private VBox createSideMenu() {
+
+        Button btn = new Button("Fischen");
+
+
+        VBox sideMenuButtons = new VBox(5);
+        sideMenuButtons.getChildren().addAll(btn);
+        //sideMenuButtons.getChildren().addAll(sidebar.getControlButton(), iconOption);
+        //sideMenuButtons.setPadding(new Insets(35, 5, 10, 5));
+
+
+        return sideMenuButtons;
+    }
+
+    private HBox createFinalHeaderBox() throws InterruptedException, ExecutionException, MessagingException {
+
+        HBox headerBox = new HBox(2);
+        headerBox.setPadding(new Insets(10, 10, 10, 10));
+        headerBox.getStyleClass().add("vbox");
+        headerBox.getChildren().addAll(createHeaderTextFields(), createSendButton());
+
+        return headerBox;
+    }
+
+
+
+
+
     private TextArea createTextArea() {
 
         mailTextArea = new TextArea();
         mailTextArea.setPrefSize(Double.MAX_EXPONENT, Double.MAX_EXPONENT);
         mailTextArea.setFocusTraversable(true);
+        mailTextArea.setWrapText(true);
+        mailTextArea.setStyle("-fx-control-inner-background: black; -fx-text-fill: white; -fx-border-color: #c0c0c0; -fx-border-width: 0px;"); //set as ID with styling.css!
         //mailTextArea.requestFocus();
+        mailTextArea.getStyleClass().add("vbox");
 
         return mailTextArea;
+    }
+
+
+    private VBox createHeaderTextFields() {
+
+        mailAdress = new TextField();
+        mailAdress.setPromptText("Type in E-Mail-Adress");
+
+        mailSubject = new TextField();
+        mailSubject.setPromptText("Subject:");
+
+        VBox headerVBox = new VBox(2);
+        headerVBox.setPrefWidth(Double.MAX_EXPONENT);
+        headerVBox.getChildren().addAll(mailAdress, mailSubject);
+
+        return headerVBox;
     }
 
     private HBox createHeaderBox() throws MessagingException, ExecutionException, InterruptedException {
@@ -166,7 +227,7 @@ public class oop2 extends Application {
                 System.out.println("Style: " + mailTextArea.getStyle());
                 Text sctext = new Text("e-Mail sent.");
                 //sctext.setStyle("-fx-alignment: center; -fx-effect: dropshadow(gaussian, black, 10, 1.0, 100, 100); -fx-fill-width: true;" +
-                        //" -fx-opacity: 2; -fx-text-alignment: center; -fx-text-fill: green");
+                //" -fx-opacity: 2; -fx-text-alignment: center; -fx-text-fill: green");
                 mailTextArea.setText(sctext.getText());
                 mailAdress.clear();
                 mailSubject.clear();
@@ -176,27 +237,14 @@ public class oop2 extends Application {
 
 
 
-
             }
         });
 
+        sendButton.setMinSize(50, 60);
 
         return sendButton;
 
     }
 
-    private VBox createHeaderTextFields() {
 
-        mailAdress = new TextField();
-        mailAdress.setPromptText("Type in E-Mail-Adress");
-
-        mailSubject = new TextField();
-        mailSubject.setPromptText("Subject:");
-
-        VBox headerVBox = new VBox(2);
-        headerVBox.setPrefWidth(Double.MAX_EXPONENT);
-        headerVBox.getChildren().addAll(mailAdress, mailSubject);
-
-        return headerVBox;
-    }
 }
