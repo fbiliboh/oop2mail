a7f131eacf2ca6d1dba69858b9f7498230ac64c3
TylerTheHumanCompiler <mc.twist@hotmail.com>
2016-03-15 12:57:40 +0100

    Delete UserVariables.java

--- src/UserVariables.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Skynet on 15.03.2016.
- */
-public class UserVariables {
-    protected static String passwd = "12345678";
-    public static String user = "meied3";
-    public static String emailbfh = "dominicstefan.meier";
-
-}
